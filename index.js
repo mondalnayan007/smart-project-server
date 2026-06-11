f58e7b21bfdd62944edc2654c96e001af43a8f28
Nayan Mondal <n007kingkong@gmail.com>
2026-06-11 22:17:15 +0600

    statrted the server

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+
+const app = express()
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("smart server is running")
+});
+
+
+
+app.listen(port,()=>{
+    console.log(`Our server is running in ${port}`);
+})
